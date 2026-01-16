991c277c463c2d56ff896204a256ad09e5383033
SatLess <spiritlesssaturday@gmail.com>
2026-01-16 17:05:17 -0300

    Tweaks and Small fixes

--- Analise-2025.sql
+++ Analise-2025.sql
@@ -1,10 +1,10 @@
 -- Qual a quantidade de universidades por país?
-SELECT Count(`Country`) FROM `2025_Overall_Rank` GROUP BY `Country` ORDER BY COUNT(`Country`);
+SELECT `Country`, Count(`Country`) FROM `2025_Overall_Rank` GROUP BY `Country` ORDER BY COUNT(`Country`);
 
 -- Quantos % cada setor compõe em comparação com o total?
 SELECT Sector, COUNT(`Sector`), COUNT(`Sector`) * 100.0 / SUM((COUNT(`Sector`))) OVER () AS Percentage FROM `2025_Overall_Rank` GROUP BY `Sector` ORDER BY COUNT(`Sector`);
 
--- Quais foi a instituição melhor colocada no ranking? E a pior?
+-- Qual foi a instituição brasileira melhor colocada no ranking?
 SELECT tabela_original.* FROM `2025_Overall_Rank` tabela_original
     JOIN(
         SELECT `Global_Rank`, `Country`, MIN(`Global_Rank`) as MinValue FROM `2025_Overall_Rank` WHERE `Country` = 'BRA' GROUP BY `Global_Rank`
@@ -13,6 +13,7 @@ SELECT tabela_original.* FROM `2025_Overall_Rank` tabela_original
     ORDER BY tabela_original.`Global_Rank` 
     ASC LIMIT 1 ;
 
+-- E a pior?
 SELECT tabela_original.* FROM `2025_Overall_Rank` tabela_original
     JOIN(
         SELECT `Global_Rank`, `Country`, MIN(`Global_Rank`) as MinValue FROM `2025_Overall_Rank` WHERE `Country` = 'BRA' GROUP BY `Global_Rank`
